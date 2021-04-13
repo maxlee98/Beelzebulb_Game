9e9a30d9cc6525defbcd76ec24b26fde371c462f
yengsheng <64056543+yengsheng@users.noreply.github.com>
2021-04-13 16:38:36 +0800

    New Merge

--- Beelzebulb/ui.R
+++ Beelzebulb/ui.R
@@ -62,13 +62,18 @@ body <- dashboardBody(
             actionButton("login", "Login"),
             HTML("<p></p>"),
             uiOutput("buttonGameLobby")
+<<<<<<< Updated upstream
             ),
+=======
+    ),
+>>>>>>> Stashed changes
     
     tabItem(tabName = "game",
             h1("This is the Game Lobby to Game Tab"),
             verbatimTextOutput("playerturn"),
             verbatimTextOutput("gamePhase"),
             fluidRow(
+<<<<<<< Updated upstream
               # the trick here is to make the gameboard image 'position:absolute;z-order:0'; 
               # Then, to superimpose images, style them to be 'position:relative;z-order:999'
               img(src='FantasyMap.jpg',style="position:absolute;z-order:0",width="100%",height="300px"),
@@ -86,6 +91,73 @@ body <- dashboardBody(
             h2("Widgets tab content"),
             p("Instruction goes here")
           ),
+=======
+              box(
+                title = "A Very Simple Board Game",width=12,
+                htmlOutput("playercolorchoice"),
+                uiOutput("moreControls"),
+                # the trick here is to make the gameboard image 'position:absolute;z-order:0'; 
+                # Then, to superimpose images, style them to be 'position:relative;z-order:999'
+                img(src='FantasyMap.jpg',style="position:absolute;z-order:0",width="500px",height="300px"),
+                imageOutput("cell11",height="100px",width="100px",click="click11",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell12",height="100px",width="100px",click="click12",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell13",height="100px",width="100px",click="click13",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell14",height="100px",width="100px",click="click14",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell15",height="100px",width="100px",click="click15",inline=TRUE),
+                tags$br(),
+                imageOutput("cell21",height="100px",width="100px",click="click21",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell22",height="100px",width="100px",click="click22",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell23",height="100px",width="100px",click="click23",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell24",height="100px",width="100px",click="click24",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell25",height="100px",width="100px",click="click25",inline=TRUE),
+                tags$br(),
+                imageOutput("cell31",height="100px",width="100px",click="click31",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell32",height="100px",width="100px",click="click32",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell33",height="100px",width="100px",click="click33",inline=TRUE), # height and width are for the containing div, not the image itself
+                imageOutput("cell34",height="100px",width="100px",click="click34",inline=TRUE),  # height and width are for the containing div, not the image itself
+                imageOutput("cell35",height="100px",width="100px",click="click35",inline=TRUE),
+                tags$br(),
+                p("ESD Fantasy Map and Game Pieces by Tan Yi Lin")
+              )
+            ),
+            fluidRow(
+              column(1, img(src='Wire_Designs-01.png',width="100px",height="100px")),
+              column(1, "x2"),
+              column(1, img(src='Wire_Designs-02.png',width="100px",height="100px")),
+              column(1, "x2"),
+              column(1, img(src='Wire_Designs-03.png',width="100px",height="100px")),
+              column(1, "x2"),
+              column(1, img(src='Wire_Designs-04.png',width="100px",height="100px")),
+              column(1, "x2"),
+              column(1, img(src='Wire_Designs-05.png',width="100px",height="100px")),
+              column(1, "x2")
+            ),
+            
+            
+            actionButton("refreshGame", "Refresh")
+    ),
+    
+    tabItem(tabName = "instruct",
+            h2("Beelzebulb's Game Instructions"),
+            p("Welcome to Beelzebulb! A 4 Player Game in which 3 Players work together as Engineers 
+              in attempt to connect wires from the battery all the way to the light bulb! The fourth
+              player would play the role of an Imposter, whereby he tries to stop the 3 Engineers from
+              reaching the lightbulb."),
+            p("Roles of the Players would not be disclosed. It would only appear on each individual's
+              screen."),
+            h3("The Game is comprised of 3 Phases : Standby Phase, Action Phase, End Phase."),
+            h4("Standby Phase"),
+            p("In the Standby Phase, players would be given a multiple choice physics question.
+              Answering the question right allows the player to draw a card. Failure to answer correctly
+              results in the player not being able to draw a card."),
+            h4("Action Phase"),
+            p("In this phase, players would have to choose a card from their hand and place it onto the board."),
+            h4("End Phase"),
+            p("During this phase, the board status would be checked. Upon connection to the bulb or
+              the deck running out of cards, the game will end and the conclusion of the game would
+              be shown.")
+    ),
+>>>>>>> Stashed changes
     tabItem(tabName = "test",
             h2("Testing Goes Here"),
             h3("End Game Screen (Result) with a return to lobby button"),
@@ -98,8 +170,13 @@ body <- dashboardBody(
             h4("Popup with a random physics question and the multiple choice answers with submit button"),
             h3("Defeat Condition Logic"),
             h4("Running out of cards")
+<<<<<<< Updated upstream
           )
         )
+=======
+    )
+  )
+>>>>>>> Stashed changes
 )
 
 ui <- dashboardPage(
